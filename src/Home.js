cf9561d650c6a48e5c52fda41b871cd0d358fe67
m3thom <39703347+m3thom@users.noreply.github.com>
2018-06-21 16:45:58 +0700

    marker query
    lol

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,17 @@
 /*global google*/
 
 import React from "react"
-<<<<<<< HEAD
 import { compose, withProps, lifecycle, withStateHandlers } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, Polygon, Polyline } from "react-google-maps"
 import firebase, { db } from './config/Fire';
-=======
-import { compose, withProps, lifecycle } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-import firebase , {db} from './config/Fire';
-
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
 
 const _ = require("lodash");
 const { DrawingManager } = require("react-google-maps/lib/components/drawing/DrawingManager");
 const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+
+var jpath = { lat: 13.6523, lng: 100.4938 }
+
+
 
 const MapWithADrawingManager = compose(
   withProps({
@@ -26,13 +23,9 @@ const MapWithADrawingManager = compose(
   }), lifecycle({
     componentWillMount() {
       const refs = {}
-      
+
       this.setState({
-<<<<<<< HEAD
         bounds: null,
-=======
-        
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
         markers: [],
 
         onMapMounted: ref => {
@@ -55,22 +48,15 @@ const MapWithADrawingManager = compose(
           const nextMarkers = places.map(place => ({
             position: place.geometry.location,
           }));
-<<<<<<< HEAD
           const nextCenter = _.get(nextMarkers, '0.position', this.props.center);
 
-=======
-          const nextCenter = _.get(nextMarkers, '0.position', this.state.currentLatLng);
-          
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
           this.setState({
-            currentLatLng: nextCenter,
+            center: nextCenter,
             markers: nextMarkers,
           });
-          
-
         },
-          
-            
+
+
       })
 
     },
@@ -87,9 +73,8 @@ const MapWithADrawingManager = compose(
 )(props =>
   <GoogleMap
     ref={props.onMapMounted}
-    center={props.currentLatLng}
+    center={props.center}
     defaultZoom={17}
-<<<<<<< HEAD
     defaultMapTypeId={'satellite'}
     defaultOptions={{
       mapTypeControlOptions: {
@@ -100,9 +85,6 @@ const MapWithADrawingManager = compose(
   >
 
     {/*set data that get from firestore to overlay eg. draw saved data, overlayOption*/}
-    <Polygon
-    />
-
     {
       props.temp.map(polyCoords => {
         for (var key in polyCoords) {
@@ -116,17 +98,19 @@ const MapWithADrawingManager = compose(
         }
       })
     }
-
-
-    {/*same as above*/}
-    <Polyline
-      path={[
-      ]}
-    />
-
-    {/*same as above*/}
-    <Marker
-    />
+    {
+      props.tempMarker.map(markerCoords => {
+        for (var key in markerCoords) {
+          let markerObject = {}
+          var value = markerCoords[key]
+          for (var key2 in value) {
+            var value2 = value[key2]
+            markerObject = { lat: value2.latitude, lng: value2.longitude }
+          }
+          return <Marker position={markerObject} />
+        }
+      })
+    }
 
     <DrawingManager
 
@@ -138,39 +122,14 @@ const MapWithADrawingManager = compose(
         polygon.getPath().forEach(function (value) {
           console.log(value.lat(), value.lng());
           polyCoords.push({
-=======
-    
-  >
-    {props.isMarkerShown && <Marker position={props.currentLatLng} onClick={props.onMarkerClick} />}
-    
-    
-    <DrawingManager
-
-      onPolygonComplete={polygon => {
-        console.log("this polygon length: " + polygon.getPath().getLength());
-
-        var coords = [];
-
-
-        polygon.getPath().forEach(function (value, index) {
-          console.log(value.lat(), value.lng());
-          coords.push({
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
             latitude: value.lat(),
             longitude: value.lng()
           });
         });
-<<<<<<< HEAD
         //add coords array to cloud firestore
         db.collection("users").doc("userId").collection("polygon").add({
           //add data here
           polyCoords,
-=======
-
-        db.collection("users").doc("userId").collection("polygon").add({
-          //add data here
-          latlng: coords
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
         }).then(function () {
           console.log("Document successfully written!");
         })
@@ -181,7 +140,6 @@ const MapWithADrawingManager = compose(
 
       //call when polyline complete
       onPolylineComplete={polyline => {
-<<<<<<< HEAD
         var polylineCoords = []
         console.log(polyline.getPath().getLength())
 
@@ -206,59 +164,23 @@ const MapWithADrawingManager = compose(
             console.error("Error writing document: ", error);
           });
 
-=======
-        console.log("this polyline length: " + polyline.getPath().getLength());
-
-        var coords = [];
-
-
-        polyline.getPath().forEach(function (value, index) {
-          console.log(value.lat(), value.lng());
-          coords.push({
-            latitude: value.lat(),
-            longitude: value.lng()
-          });
-        });
-
-        db.collection("users").doc("userId").collection("polyline").add({
-          //add data here
-          latlng: coords
-        }).then(function () {
-          console.log("Document successfully written!");
-        })
-          .catch(function (error) {
-            console.error("Error writing document: ", error);
-          });
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
       }}
 
       //call when marker complete
       onMarkerComplete={marker => {
-<<<<<<< HEAD
 
         var markerCoords = []
         console.log(marker.getPosition())
         //store data to cloud firestore
         markerCoords.push({
-=======
-        console.log(marker.getPosition().lat(), marker.getPosition().lng())
-
-        var coords = []
-
-        coords.push({
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
           latitude: marker.getPosition().lat(),
           longitude: marker.getPosition().lng()
         });
 
         db.collection("users").doc("userId").collection("marker").add({
           //add data here
-<<<<<<< HEAD
           markerCoords,
 
-=======
-          latlng: coords
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
         }).then(function () {
           console.log("Document successfully written!");
         })
@@ -266,14 +188,7 @@ const MapWithADrawingManager = compose(
             console.error("Error writing document: ", error);
           });
       }}
-<<<<<<< HEAD
-      onRectangleComplete={rectangle => {
-        console.log(props.queryShape)
-      }}
-=======
 
-
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
       defaultOptions={{
         drawingControl: true,
         drawingControlOptions: {
@@ -283,8 +198,6 @@ const MapWithADrawingManager = compose(
             google.maps.drawing.OverlayType.POLYGON,
             google.maps.drawing.OverlayType.POLYLINE,
             google.maps.drawing.OverlayType.MARKER,
-<<<<<<< HEAD
-            google.maps.drawing.OverlayType.RECTANGLE,
           ],
         },
         polygonOptions: {
@@ -300,21 +213,10 @@ const MapWithADrawingManager = compose(
         },
         markerOption: {
         }
-=======
-
-          ],
-          polygonOptions: {
-          fillColor: '#BCDCF9',
-
-          },
-          polylineOptions: {
-
-          }
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
       }
 
+
       }
-    }
     />
     <SearchBox
       ref={props.onSearchBoxMounted}
@@ -339,21 +241,15 @@ const MapWithADrawingManager = compose(
         }}
       />
     </SearchBox>
-    {props.markers.map((marker, index) =>
-      <Marker key={index} position={marker.position} />
-    )}
-<<<<<<< HEAD
+    {
+      props.markers.map((marker, index) =>
+        <Marker key={index} position={marker.position} />
+      )
+    }
     {props.isMarkerShown && <Marker position={props.center} onClick={props.onMarkerClick} />}
   </GoogleMap>
 );
 
-=======
-    
-  </GoogleMap>
-);
-
-class App extends React.PureComponent{
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
 
 
 export default class App extends React.PureComponent {
@@ -361,19 +257,18 @@ export default class App extends React.PureComponent {
     super(props);
     this.logout = this.logout.bind(this);
     this.handleClick = this.handleClick.bind(this);
-<<<<<<< HEAD
     this.queryPolygon = this.queryPolygon.bind(this)
+    this.querymarker = this.querymarker.bind(this);
     this.state = {
       currentLatLng: {
         lat: 13.652383, lng: 100.493872
       },
       isMarkerShown: true,
       temp: [],
-      tempPolyline: [],
+      tempMarker: [],
     }
   }
   queryPolygon() {
-
     let arr = []
     db.collection("users").doc("userId").collection("polygon").get().then(function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
@@ -384,28 +279,25 @@ export default class App extends React.PureComponent {
       temp: arr
     })
   }
-
-
+  querymarker() {
+    let arr = []
+    db.collection("users").doc("userId").collection("marker").get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        arr.push(doc.data())
+      });
+    });
+    this.setState({
+      tempMarker: arr
+    })
+  }
 
   handleClick() {
-=======
-    this.state = {
-     currentLatLng:{
-      lat: 13.652383, lng: 100.493872
-     },
-      isMarkerShown : true
-    }
-    
-  
-  }
-  saveMap() {
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
-
     this.getGeoLocation();
     this.handleMarkerClick();
     this.handleMarkerClick2();
+    this.querymarker();
+    this.queryPolygon();
   }
-<<<<<<< HEAD
   getGeoLocation = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
@@ -421,34 +313,6 @@ export default class App extends React.PureComponent {
     )
   }
 
-  componentDidMount() {
-
-    this.handleClick();
-=======
-  handleClick(){
-    
-    this.getGeoLocation();
-    this.handleMarkerClick();
-    this.handleMarkerClick2();
-  }
-
-  
-  
-  getGeoLocation = () => {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                console.log(position.coords);
-                this.setState(prevState => ({
-                    currentLatLng: {
-                        ...prevState.currentLatLng,
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    }
-                }))
-            }
-        )
-    }
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
 
   componentDidMount() {
     this.handleClick();
@@ -469,53 +333,25 @@ export default class App extends React.PureComponent {
     this.setState({ user: null });
   }
   handleMarkerClick = () => {
-<<<<<<< HEAD
     this.setState({ isMarkerShown: true })
   }
   render() {
 
     return (
       <div>
-=======
-    
-      this.setState({ isMarkerShown: true })
-    
-  }
-
-  render() {
-
-    return (
-      
-
-      <div>
-        
-        <h2>Map Creation</h2><br />
-        <MapWithADrawingManager
-        currentLatLng={this.state.currentLatLng}
-        isMarkerShown={this.state.isMarkerShown}
-         /><br /><br />
-        <button onClick={this.handleClick}  className="btn btn-info">Find yourself</button><br /><br />
-        <button onClick={this.saveMap} className="btn btn-success">Save Map</button>
-        <button onClick={this.logout} style={{ marginLeft: '25px' }} className="btn btn-primary">Logout</button>
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
 
         <MapWithADrawingManager
           center={this.state.center}
           isMarkerShown={this.state.isMarkerShown}
           temp={this.state.temp}
-          tempPolyline={this.state.tempPolyline}
+          tempMarker={this.state.tempMarker}
 
         /><br /><br />
         <button onClick={this.handleClick} className="btn btn-info">Find yourself</button><br /><br />
         <button onClick={this.queryPolygon} className="btn btn-success">Get shape</button>
+        <button onClick={this.querymarker} className="btn btn-success">Get shape marker</button>
         <button onClick={this.logout} style={{ marginLeft: '25px' }} className="btn btn-primary">Logout</button>
       </div>
     )
   }
-<<<<<<< HEAD
 }
-=======
-}
-
-export default  App 
->>>>>>> 070bde1eb546e9cb2f6dafb65624f4591f2bd460
